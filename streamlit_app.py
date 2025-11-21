14f566c45756a5821dd947a5b94bd0a734343f36
MunavarshoA <abdullaev.moonavar@gmail.com>
2025-11-21 18:00:57 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit as st
-
+import pandas as pd 
 st.title('🎈 MY FRIST PROJECT')
 
 st.write('LETS GO!')
 
 with st.expander("DATA"):
   st.write('**OUR DATA**')
-  
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df
