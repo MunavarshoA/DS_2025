71612bc4ef118f033b8c41bf6b6eb2bc3053c9f6
MunavarshoA <abdullaev.moonavar@gmail.com>
2025-11-21 18:08:40 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,6 @@ with st.expander("DATA"):
   st.write('**OUR DATA**')
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
   df
+
+with st.expander("Visualization"):
+  st.scatter_chart(data=df , x='bill_length_mm' , y='bill_depth_mm', color='species')
