7770f6f31dc1fe12fe66b7e5d07e6ba091be07c1
MunavarshoA <abdullaev.moonavar@gmail.com>
2025-11-24 16:54:43 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,9 @@ with st.expander("DATA"):
 with st.expander("Visualization"):
   st.scatter_chart(data=df , x='bill_length_mm' , y='bill_depth_mm', color='species')
 
+X_raw = df.drop('species', axis=1)
+y_raw = df.species
+
 with st.sidebar:
   st.header('Input features')
   island = st.selectbox('Island', ('Torgersen', 'Dream', ' Biscoe'))
@@ -20,3 +23,17 @@ with st.sidebar:
   flipper_length_mm = st.slider("**flipper_length_mm**", 32.1, 59.6, 55.1)
   body_mass_g = st.slider("**body_mass_g**", 2700, 6300, 4207)
   sex = st.selectbox('**Gender**', ('male', 'female'))
+
+data = {'island':island,
+        'bill_length_mm': bill_length_mm,
+        'bill_depth_mm': bill_depth_mm,
+        'flipper_length_mm': flipper_length_mm,
+        'body_mass_g' : body_mass_g,
+        'sex' : 'sex}
+input_df = pd.DataFrame(data, index=[0])
+input_penguins = pd.concat([input_df, X_raw],axis=0)
+
+        
+        
+        
+
